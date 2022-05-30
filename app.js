a5b4e0240eed2e95bcec177b161868bacf501e1c
BarelySmooth <83728060+BarelySmooth@users.noreply.github.com>
2022-05-30 19:03:04 +0400

    Update Rebot About

--- app.js
+++ app.js
@@ -57,8 +57,25 @@ client.on("channelCreate", async (channel) => {
 
 client.on("messageCreate", async (message) => {
   if (message.content === "!rebot uptime") {
+    // Code from https://stackoverflow.com/q/36098913
+    function secondsToDays(seconds) {
+      seconds = Number(seconds);
+      let d = Math.floor(seconds / (3600 * 24));
+      let h = Math.floor((seconds % (3600 * 24)) / 3600);
+      let m = Math.floor((seconds % 3600) / 60);
+      let s = Math.floor(seconds % 60);
+
+      let dDisplay = d > 0 ? d + (d == 1 ? " day, " : " days, ") : "";
+      let hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
+      let mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
+      let sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
+      return dDisplay + hDisplay + mDisplay + sDisplay;
+    }
+
     message.channel.send(
-      `Uptime: ${Math.round(process.uptime() / 60)} minutes`
+      `In ${client.guilds.cache.size} servers!\nUptime: ${secondsToDays(
+        Math.floor(process.uptime())
+      )}`
     );
   } else if (message.content === "!rebot help") {
     message.channel.send(
