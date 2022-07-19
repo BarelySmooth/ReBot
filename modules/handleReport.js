af09484d5a4f8daad0f60e142de6678bc5914b26
BarelySmooth <83728060+BarelySmooth@users.noreply.github.com>
2022-07-19 10:56:11 +0400

    Change how multiple reports of the same message are handled
    - If the same person reports the message twice, no changes will be made and the user will be told that said report was already sent.
    
    - If another person reports the same message, their user tag will be appended to the "Reported by" field in the report embed.

--- modules/handleReport.js
+++ modules/handleReport.js
@@ -52,51 +52,90 @@ async function postReport(modalInteraction, interaction) {
       ephemeral: true,
       components: [createChannelButtonRow],
     });
-  } else {
-    try {
-      await modChannel.send({
-        embeds: [
-          new MessageEmbed()
-            .setColor("#ff0000")
-            .setDescription(interaction.targetMessage.content + "\n\n")
-            .setAuthor({
-              name: interaction.targetMessage.author.tag,
-              iconURL: interaction.targetMessage.author.displayAvatarURL(),
-            })
-            .addField("Reported by", `<@!${interaction.user.id}>`, true)
-            .addField(
-              "Reason provided?",
-              modalInteraction.fields.getTextInputValue("reason_field"),
-              true
-            )
-            .setFooter(interaction.targetMessage.author.id)
-            .setTimestamp(),
-        ],
-        components: [optionsRow1, optionsRow2(interaction)],
-      });
-    } catch (error) {
-      console.log(error.code);
+  }
 
-      if (error.code === 50001 || error.code === 50013) {
-        return modalInteraction.reply({
-          content:
-            "The bot wasn't set up correctly.\nIf you are a server admin, please make sure the bot has permission to post messages in the #rebot-reports channel!",
-          ephemeral: true,
-        });
+  let interactionReplied = false;
+  // default limit is 50 anyway, just making this clearer
+  // prettier-ignore
+  await modChannel.messages.fetch({ limit: 50 }).then((messages) => {
+    messages.forEach((message) => {
+      // checking whether the message was posted by the bot itself
+      // message.client.user.id will return the bot's account ID
+      if (message.author.id != message.client.user.id) return;
+
+      // get the last embed on the message...
+      const reportEmbed = message.embeds[message.embeds.length - 1]
+
+      // checking if the desciption of the embed is the same as that of the reported message AND if the message being reported has the same message ID as the one on the embed      
+      if ((reportEmbed?.description === interaction.targetMessage.content) && (message.components[1].components[1].url === interaction.targetMessage.url)) {
+        // making sure the same user is not reporting the message again
+        if (!reportEmbed.fields[0].value.includes(interaction.user.id)) {
+          reportEmbed.fields[0].value += `\n<@!${interaction.user.id}>`;
+          message.edit({
+            embeds: [reportEmbed],
+          });
+
+          interactionReplied = true
+          return modalInteraction.reply({
+            content: "This message has been reported to server moderators!",
+            ephemeral: true,
+          })
+        } else {
+          interactionReplied = true
+          return modalInteraction.reply({
+            content: "You've already reported this message!",
+            ephemeral: true,
+          });
+        }
       }
+    });
+  });
 
+  if (interactionReplied) return;
+
+  try {
+    await modChannel.send({
+      embeds: [
+        new MessageEmbed()
+          .setColor("#ff0000")
+          .setDescription(interaction.targetMessage.content + "\n\n")
+          .setAuthor({
+            name: interaction.targetMessage.author.tag,
+            iconURL: interaction.targetMessage.author.displayAvatarURL(),
+          })
+          .addField("Reported by", `<@!${interaction.user.id}>`, true)
+          .addField(
+            "Reason provided?",
+            modalInteraction.fields.getTextInputValue("reason_field"),
+            true
+          )
+          .setFooter(interaction.targetMessage.author.id)
+          .setTimestamp(),
+      ],
+      components: [optionsRow1, optionsRow2(interaction)],
+    });
+  } catch (error) {
+    console.log(error.code);
+
+    if (error.code === 50001 || error.code === 50013) {
       return modalInteraction.reply({
         content:
-          "Sorry, something went wrong. Please try again later or contact the server admin.",
+          "The bot wasn't set up correctly.\nIf you are a server admin, please make sure the bot has permission to post messages in the #rebot-reports channel!",
         ephemeral: true,
       });
     }
 
     return modalInteraction.reply({
-      content: "This message has been reported to server moderators!",
+      content:
+        "Sorry, something went wrong. Please try again later or contact the server admin.",
       ephemeral: true,
     });
   }
+
+  return modalInteraction.reply({
+    content: "This message has been reported to server moderators!",
+    ephemeral: true,
+  });
 }
 
 export async function showReportModal(interaction) {
